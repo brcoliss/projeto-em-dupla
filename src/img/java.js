f173b6796a47fbb17d6df178f1f9828b40762a2e
DIMITRI ITIOKA <dimitri.itioka@senacsp.edu.br>
2025-08-14 22:00:29 -0300

    coommiti inical

---
+++ src/img/java.js
@@ -0,0 +1,20 @@
+
+
+  const frases = [
+    "Seja você mesmo",
+    "Brilhe todos os dias",
+    "Sua beleza, nosso cuidado",
+    "Estilo que combina com você"
+  ];
+
+  let indice = 0;
+  const elemento = document.getElementById("frase");
+
+  function trocarFrase() {
+    elemento.textContent = frases[indice];
+    indice = (indice + 1) % frases.length; // volta ao início
+  }
+
+  // Troca a frase a cada 3 segundos
+  trocarFrase(); // mostra a primeira imediatamente
+  setInterval(trocarFrase, 3000);
